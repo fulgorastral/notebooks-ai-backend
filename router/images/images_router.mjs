a0fb02d6a43cda19cbd06f25b01226be61c4c9b5
PortiESP <47105148+PortiESP@users.noreply.github.com>
2025-01-09 12:48:23 +0100

    added error handler

--- router/images/images_router.mjs
+++ router/images/images_router.mjs
@@ -9,33 +9,45 @@ router.get("/", async (req, res) => {
     const query = req.query.q
 
     // Get from Unsplash
-    const responseUnsplash = await unsplash.search.getPhotos({
-        query,
-        perPage: 3,
-    })
-    if (responseUnsplash.type === "error") {
-        if (responseUnsplash.status === 400) {
-            res.status(400).json({ error: responseUnsplash.error })
+    let normUnsplash = []
+    try{
+        const responseUnsplash = await unsplash.search.getPhotos({
+            query,
+            perPage: 9,
+        })
+        if (responseUnsplash.type === "error") {
+            if (responseUnsplash.status === 400) {
+                res.status(400).json({ error: responseUnsplash.error })
+                return
+            }
+            res.status(500).json({ error:  "Internal server error" })
             return
         }
-        res.status(500).json({ error:  "Internal server error" })
-        return
+        normUnsplash = normalizeResultsUnsplash(responseUnsplash)
+    } 
+    catch (error) {
+        console.error(`Error getting images from Unsplash: ${error}`)
     }
-    const normUnsplash = normalizeResultsUnsplash(responseUnsplash)
 
     // Get from Freepik
-    const responseFreepik = await queryFreepikAPI({ term: query })
-    if (responseFreepik.status === 400) {
-        res.status(400).json({ error: responseFreepik.error })
-        return
+    let normFreepik = []
+    try{
+        const responseFreepik = await queryFreepikAPI({ term: query })
+        if (responseFreepik.status === 400) {
+            res.status(400).json({ error: responseFreepik.error })
+            return
+        }
+        if (responseFreepik.status === 500) {
+            res.status(500).json({ error: "Internal server error" })
+            return
+        }
+    
+        const dataFreepik = await responseFreepik.json()
+        normFreepik = normalizeResultsFreepik(dataFreepik)
+    } 
+    catch (error) {
+        console.error(`Error getting images from Freepik: ${error}`)
     }
-    if (responseFreepik.status === 500) {
-        res.status(500).json({ error: "Internal server error" })
-        return
-    }
-
-    const dataFreepik = await responseFreepik.json()
-    const normFreepik = normalizeResultsFreepik(dataFreepik)
 
     res.json([...normUnsplash, ...normFreepik])
 })
